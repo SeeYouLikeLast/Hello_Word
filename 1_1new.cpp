4bddb5af75db4fb42f22602e9e552ccd44707769
SeeYouLikeLast <2698508450@qq.com>
2025-07-03 14:49:04 +0800

    Add files via upload

---
+++ 1_1new.cpp
@@ -0,0 +1,41 @@
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//void test(void)
+//{
+//	int* p = new int(10);
+//	cout << *p << endl;
+//	delete p;
+//
+//	int* arr = new int[10];
+//	for (int i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		cout << arr[i] << '\t';
+//	}
+//	delete []arr;
+//
+//}
+//
+////int main()
+////{
+////	//test();
+////
+////	int num1 = 10;
+////	int& num2 = num1;
+////	cout << "num1 = " << num1 << endl;
+////	cout << "num2 = " << num2 << endl;
+////	num2 = 100;
+////	cout << "num1 = " << num1 << endl;
+////	cout << "num2 = " << num2 << endl;
+////	int num3 = 200;
+////	num2 = num3;	// 赋值操作，非改变引用
+////	cout << "num1 = " << num1 << endl;
+////	cout << "num2 = " << num2 << endl;
+////	cout << "num3 = " << num3 << endl;
+////
+////	system("pause");
+////	return 0;
+////}
